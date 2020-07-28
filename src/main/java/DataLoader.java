7778153b53ab80ecefd7dba51b9ad43cb0ebe2b3
Simon Gavins Lee <ucabbl4@ucl.ac.uk>
2020-07-29 00:00:27 +0100

    Update DataLoader for large data files

--- src/main/java/DataLoader.java
+++ src/main/java/DataLoader.java
@@ -1,55 +1,56 @@
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Iterator;
 
 public class DataLoader {
-    String Result_File = "src/data/result2.csv";
+    Path Result_File = Paths.get("src/data/result2.csv");
+    Path Source_File = Paths.get("src/data/functions.json");
+
+    void processLine(String line) throws IOException {
+        JSONObject jsonObject = new JSONObject("{" + line + "}");
+        System.out.println(jsonObject);
+
+        Iterator<String> keys = jsonObject.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+
+            Inspect inspect = new Inspect(jsonObject.getString(key));
+            inspect.ASTMutate();
+
+            String contentToAppend = "\n" + key + "," + inspect.Original + "," +
+                    inspect.Original_Prediction + "," + inspect.UnequalToName + "," +
+                    inspect.UnequalToPrediction + "," + inspect.Total;
+            Files.write(
+                    Result_File,
+                    contentToAppend.getBytes(),
+                    StandardOpenOption.APPEND);
+        }
+    }
 
     public void load() {
         try {
-            BufferedReader br = new BufferedReader(new FileReader("src/data/functions_copy.json"));
-            String thisLine;
+            Files.write(
+                    Result_File,
+                    "Key,OriginalName,OriginalPrediction,UnequalToName,UnequalToPrediction,Total".getBytes(),
+                    StandardOpenOption.CREATE);
 
-            while ((thisLine = br.readLine()) != null) {
-                int len = thisLine.length();
-
-                // get rid of meaningless lines
-                if (len > 5) {
-                    JSONObject jsonObject = new JSONObject("{" + thisLine + "}");
-                    System.out.println(jsonObject);
-
-                    try {
-                        FileWriter myWriter = new FileWriter(Result_File);
-                        myWriter.write(
-                                "Key,OriginalName,OriginalPrediction,UnequalToName,UnequalToPrediction,Total");
-                        myWriter.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    Iterator<String> keys = jsonObject.keys();
-                    while (keys.hasNext()) {
-                        String key = keys.next();
-
-                        Inspect inspect = new Inspect(jsonObject.getString(key));
-                        inspect.ASTMutate();
-
-                        String contentToAppend = "\n" + key + "," + inspect.Original + "," +
-                                inspect.Original_Prediction + "," + inspect.UnequalToName + "," +
-                                inspect.UnequalToPrediction + "," + inspect.Total;
-                        Files.write(
-                                Paths.get(Result_File),
-                                contentToAppend.getBytes(),
-                                StandardOpenOption.APPEND);
-                    }
-                }
+            try {
+                Files.lines(Source_File)
+                        .filter(line -> line.length() > 5)
+                        .forEach(line -> {
+                            try {
+                                processLine(line);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         } catch (Exception e) {
             e.printStackTrace();
